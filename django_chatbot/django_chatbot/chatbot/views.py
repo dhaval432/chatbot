fade341ed814ccc82b57a30142057f521198c6cb
dhaval432 <dp780944@gmail.com>
2023-12-30 21:39:58 +0530

    redirect

--- django_chatbot/django_chatbot/chatbot/views.py
+++ django_chatbot/django_chatbot/chatbot/views.py
@@ -1,33 +1,85 @@
-# chatbot_app/views.py
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from .models import Chat
-import openai
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib import messages
+from django.contrib import auth
+from django.contrib.auth.models import User
+from chatbot.models import Chat
+
+from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+
+
 
 def ask_openai(message):
-    # Use OpenAI API to get a response
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=message,
-        temperature=0.7,
-        max_tokens=150,
-        n=1,
-        stop=None,
+    response = openai.ChatCompletion.create(
+        model = "gpt-4",
+        messages=[
+            {"role": "system", "content": "You are an helpful assistant."},
+            {"role": "user", "content": message},
+        ]
     )
-    return response.choices[0].text.strip()
+    
+    answer = response.choices[0].message.content.strip()
+    return answer
 
+# Create your views here.
 def chatbot(request):
     if request.user.is_authenticated:
-        chats = Chat.objects.filter(user=request.user)
+        try:
+            chats = Chat.objects.filter(user=request.user)
+        except ObjectDoesNotExist:
+            chats = None
 
         if request.method == 'POST':
             message = request.POST.get('message')
             response = ask_openai(message)
 
-            chat = Chat(user=request.user, message=message, response=response)
+            chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
             chat.save()
             return JsonResponse({'message': message, 'response': response})
 
-        return render(request, 'chatbot_app/chatbot.html', {'chats': chats})
+        return render(request, 'chatbot.html', {'chats': chats})
 
-  
+    # If the user is not authenticated and trying to access the chatbot page, redirect to the login page
+    if request.path == '/chatbot/':
+        return redirect('login')  # Adjust 'login' to your actual login URL
+
+    return redirect('chatbot')
+# def login(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = auth.authenticate(request, username=username, password=password)
+#         if user is not None:
+#             auth.login(request, user)
+#             return redirect('chatbot')
+#         else:
+#             error_message = 'Invalid username or password'
+#             return render(request, 'login.html', {'error_message': error_message})
+#     else:
+    
+#         return render(request, 'login.html')
+
+
+# def logout_page(request):
+#     logout(request, )
+#     return redirect('login.html')
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Account created for {username}!')
+#             return redirect('login')
+#         else:
+#             messages.error(request, 'Error in registration. Please check the form.')
+
+#     else:
+#         form = UserCreationForm()
+        
+#     return render(request, 'register.html', {'form': form})
+
+
